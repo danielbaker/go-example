101370df365b5f39162bc447ebfc2779f15b8fd6
Dan Baker <dan.baker@airwallex.com>
2022-07-27 11:31:33 +1000

    remove unneeded artifact upload

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,6 +9,7 @@ buildkite-agent artifact download output.txt .
 EXPECTED_VALUE="Hello, $NAME!"
 
 if [ "$EXPECTED_VALUE" == "$(cat output.txt)" ] ;then
+    echo "Correct output detected: $(cat output.txt)"
     exit 0
 else
     echo "Unexpected output detected: $(cat output.txt)"
